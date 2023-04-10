93b131f04007bdf128a67d354d0b6305c9f99ea2
Johann <johannfazakas@gmail.com>
2023-04-10 11:50:41 +0300

    code build builds and uploads image to ECR

--- scripts/create-ci-cd-stack.sh
+++ scripts/create-ci-cd-stack.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+export GITHUB_TOKEN='REPLACE_ME'
+
 aws cloudformation create-stack \
   --stack-name stuff-ci-cd-stack \
   --template-body file://cloudformation/stuff-ci-cd-template.yml \
-  --parameters ParameterKey=GitHubOAuthToken,ParameterValue={REPLACE_ME_WITH_YOUR_TOKEN} \
-  --capabilities CAPABILITY_IAM
+  --parameters ParameterKey=GitHubOAuthToken,ParameterValue="$GITHUB_TOKEN" \
+  --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM
--- scripts/update-ci-cd-stack.sh
+++ scripts/update-ci-cd-stack.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-export GITHUB_TOKEN='replace me'
+export GITHUB_TOKEN='REPLACE_ME'
 
 aws cloudformation update-stack \
   --stack-name stuff-ci-cd-stack \
   --template-body file://cloudformation/stuff-ci-cd-template.yml \
   --parameters ParameterKey=GitHubOAuthToken,ParameterValue="$GITHUB_TOKEN" \
-  --capabilities CAPABILITY_IAM
+  --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM
