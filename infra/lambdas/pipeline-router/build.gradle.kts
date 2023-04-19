fe4a0e487840515f0e9e22d92005c918c6dc2b10
Johann <johannfazakas@gmail.com>
2023-04-19 17:46:41 +0300

    stuff pipeline router lambda selectively triggers pipelines based on changeset

--- infra/lambdas/pipeline-router/build.gradle.kts
+++ infra/lambdas/pipeline-router/build.gradle.kts
@@ -9,6 +9,7 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.0")
     implementation("com.amazonaws:aws-lambda-java-core:1.2.2")
     implementation("com.amazonaws:aws-lambda-java-events:3.11.1")
     implementation("com.amazonaws:aws-java-sdk-codepipeline:1.12.450")
--- infra/lambdas/pipeline-router/src/main/kotlin/ro/johann/stuff/infra/pipelinerouter/StuffPipelineRouter.kt
+++ infra/lambdas/pipeline-router/src/main/kotlin/ro/johann/stuff/infra/pipelinerouter/StuffPipelineRouter.kt
@@ -6,31 +6,86 @@ import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.RequestHandler
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 
 class StuffPipelineRouter : RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
-    private val codePipelineClient = AWSCodePipelineClientBuilder.defaultClient()
-    private val objectMapper = ObjectMapper()
+    private companion object {
+        private val codePipelineClient = AWSCodePipelineClientBuilder.defaultClient()
+        private val objectMapper = ObjectMapper()
+        private val services = listOf(
+            StuffService(
+                name = "stuff-web-app",
+                pipelineName = "stuff-web-app-code-pipeline",
+                rootFolder = "web-app/"
+            )
+        )
+    }
 
     override fun handleRequest(input: APIGatewayProxyRequestEvent, context: Context): APIGatewayProxyResponseEvent {
-        val logger = context.logger
-        val pipelineName = "stuff-web-app-code-pipeline"
-        logger.log("Will trigger pipeline $pipelineName. Input: ${input.body}.")
+        val pushEvent = objectMapper.readValue<GitHubPushEvent>(input.body)
+        context.logger.log("Pipeline router triggered by push event: $pushEvent.")
+        val changes = pushEvent.commits.asSequence()
+            .flatMap(GitHubCommit::changedFiles)
+            .toSet()
 
-        val result = codePipelineClient.startPipelineExecution(StartPipelineExecutionRequest().withName(pipelineName))
-        val response = PipelineRoutingResponse(pipelineName, result.pipelineExecutionId)
-
-        logger.log("Pipeline $pipelineName triggered successfully. Execution id: ${result.pipelineExecutionId}")
+        val pipelineExecutions = services
+            .filter { service -> changes.any { it.startsWith(service.rootFolder) } }
+            .map {
+                context.logger.log("Service ${it.name} modified. Triggering pipeline ${it.pipelineName}.")
+                val startPipelineExecution =
+                    codePipelineClient.startPipelineExecution(StartPipelineExecutionRequest().withName(it.pipelineName))
+                PipelineExecution(it.pipelineName, startPipelineExecution.pipelineExecutionId)
+            }
+        context.logger.log("Pipeline router finished. Triggered pipelines: $pipelineExecutions.")
 
         return APIGatewayProxyResponseEvent()
             .withStatusCode(200)
             .withHeaders(mapOf("Content-Type" to "application/json"))
-            .withBody(objectMapper.writeValueAsString(response))
+            .withBody(objectMapper.writeValueAsString(PipelineRoutingResponse(pipelineExecutions)))
     }
 }
 
-private data class PipelineRoutingResponse(
+@JsonIgnoreProperties(ignoreUnknown = true)
+private data class GitHubPushEvent @JsonCreator constructor(
+    @JsonProperty("ref") val ref: String,
+    @JsonProperty("before") val before: String,
+    @JsonProperty("after") val after: String,
+    @JsonProperty("repository") val repository: GitHubRepository,
+    @JsonProperty("commits") val commits: List<GitHubCommit>,
+)
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+private data class GitHubRepository @JsonCreator constructor(
+    @JsonProperty("id") val id: Long,
+    @JsonProperty("name") val name: String,
+)
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+private data class GitHubCommit @JsonCreator constructor(
+    @JsonProperty("id") val id: String,
+    @JsonProperty("modified") val modifiedFiles: List<String>,
+    @JsonProperty("added") val addedFiles: List<String>,
+    @JsonProperty("removed") val removedFiles: List<String>,
+) {
+    val changedFiles = modifiedFiles + addedFiles + removedFiles
+}
+
+private data class StuffService(
+    val name: String,
+    val rootFolder: String,
     val pipelineName: String,
-    val executionId: String,
+)
+
+private data class PipelineRoutingResponse(
+    val pipelinesStarted: List<PipelineExecution>
+)
+
+private data class PipelineExecution(
+    val pipelineName: String,
+    val executionId: String
 )
