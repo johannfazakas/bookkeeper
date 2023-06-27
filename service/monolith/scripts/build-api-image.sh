ecf6e42401bc84e8d82e344df5f239d2dbe7775b
Johann Fazakas <johannfazakas@gmail.com>
2023-06-27 08:05:33 +0300

    #9 updated image name in build phase

--- service/monolith/scripts/build-api-image.sh
+++ service/monolith/scripts/build-api-image.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 export DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+export AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query 'Account' --output text)
 
 cd $DIR/../
-./gradlew clean bootBuildImage
+./gradlew clean bootBuildImage --imageName=$AWS_ACCOUNT_ID.dkr.ecr.eu-central-1.amazonaws.com/bookkeeper/api
 
 echo "Api image built."
