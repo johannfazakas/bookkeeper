3e83a085453b8e342c08abf98d81f7b47b03eb65
Johann Fazakas <johannfazakas@gmail.com>
2023-07-26 16:47:44 +0300

    added initial api gateway ktor service

---
+++ service/api-gateway/src/main/kotlin/apigw/bk/jf/ro/web/Routing.kt
@@ -0,0 +1,13 @@
+package apigw.bk.jf.ro.web
+
+import io.ktor.server.routing.*
+import io.ktor.server.response.*
+import io.ktor.server.application.*
+
+fun Application.configureRouting() {
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+    }
+}
---
+++ service/api-gateway/src/main/kotlin/apigw/bk/jf/ro/web/Serialization.kt
@@ -0,0 +1,11 @@
+package apigw.bk.jf.ro.web
+
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.*
+
+fun Application.configureSerialization() {
+    install(ContentNegotiation) {
+        json()
+    }
+}
