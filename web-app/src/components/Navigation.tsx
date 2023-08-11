52f4f96bde5a754d86ab3394dd311380e28e2482
Johann Fazakas <johannfazakas@gmail.com>
2023-08-11 18:05:51 +0300

    integrated material ui in web app

--- web-app/src/components/Navigation.tsx
+++ web-app/src/components/Navigation.tsx
@@ -1,17 +1,31 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import {AppBar, Button, Toolbar, Typography} from "@mui/material";
 
-function Navigation() {
+const Navigation: React.FC = () => {
+    const [isLogged, setIsLogged] = React.useState(true)
+
     return (
-        <nav>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/accounts">Accounts</Link></li>
-                <li><Link to="/income">Income</Link></li>
-                <li><Link to="/expenses">Expenses</Link></li>
-                <li><Link to="/investments">Investments</Link></li>
-            </ul>
-        </nav>
+        <AppBar position="static">
+            <Toolbar variant="regular">
+                <Typography variant="h6" noWrap>
+                    Bookkeeper
+                </Typography>
+                <div>
+                    <Button color="inherit" component={Link} to="/">Home</Button>
+                    <Button color="inherit" component={Link} to="/accounts">Accounts</Button>
+                    <Button color="inherit" component={Link} to="/income">Income</Button>
+                    <Button color="inherit" component={Link} to="/expenses">Expenses</Button>
+                    <Button color="inherit" component={Link} to="/investments">Investments</Button>
+                </div>
+                <div style={{marginLeft: 'auto'}}>
+                    {!isLogged
+                        ? (<Button color="inherit">Login</Button>)
+                        : (<Button color="inherit">Logout</Button>)
+                    }
+                </div>
+            </Toolbar>
+        </AppBar>
     );
 }
 
